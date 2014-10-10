7fc45f998b824332b8b690d46bda877ac3e2d020
Damiano <damianopiovesan@gmail.com>
2014-10-10 11:35:35 +0200

    Doxygen homepage

---
+++ homepage.h
@@ -0,0 +1,11 @@
+/**@mainpage Basic information
+ *
+
+ * The Victor library (Virtual Construction Toolkit for Proteins) is an open-source project dedicated to providing tools for analyzing and manipulating protein structures. 
+ * It contains methods that will allow you to representate one aminoacidic string in a efficient way. 
+ * 
+ * For a general introduction, tutorial and usage examples please visit the following wiki http://protein.bio.unipd.it/victor
+ *
+ * In the Victor wiki you will also find instructions to run the implemented Unit tests.
+ * 
+ */
